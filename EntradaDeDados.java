7ffa2bf5e8c7eef62e3dd6af382d58dcc15f9e26
Kaio Alexandre <86897929+KaioDoNascimento@users.noreply.github.com>
2022-07-05 20:56:40 -0300

    Update EntradaDeDados.java

--- EntradaDeDados.java
+++ EntradaDeDados.java
@@ -8,7 +8,7 @@ public class EntradaDeDados {
         Scanner scnr = new Scanner(System.in);
         System.out.print("Digite um número inteiro a ser convertido para binário: ");
         int num = scnr.nextInt();
-        System.out.print("O valor inteiro "+num+" convertido em número binário é = "+Converter.toBinary(num));
+        System.out.print("O número inteiro "+num+" convertido em número binário é = "+Converter.toBinary(num));
         System.out.println();
     }
    
